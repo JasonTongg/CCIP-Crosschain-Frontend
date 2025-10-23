e49144b78a80a730a4e7f0d4a17923c1a6cb04c8
JasonTongg <aboytong123@gmail.com>
2025-10-23 10:28:40 +0700

    change connect button to light theme and blue color

--- pages/_app.js
+++ pages/_app.js
@@ -4,36 +4,57 @@ import { Provider } from "react-redux";
 import Store from "../store/store";
 import "@rainbow-me/rainbowkit/styles.css";
 import {
-  getDefaultConfig,
-  RainbowKitProvider,
-  darkTheme,
+	getDefaultConfig,
+	RainbowKitProvider,
+	lightTheme,
 } from "@rainbow-me/rainbowkit";
 import { WagmiProvider } from "wagmi";
-import { sepolia, optimismSepolia, baseSepolia, arbitrumSepolia, unichainSepolia, soneiumMinato } from "wagmi/chains";
+import {
+	sepolia,
+	optimismSepolia,
+	baseSepolia,
+	arbitrumSepolia,
+	unichainSepolia,
+	soneiumMinato,
+} from "wagmi/chains";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 const config = getDefaultConfig({
-  appName: "My RainbowKit App",
-  projectId: "0e50ad124798913a4af212355f956d06",
-  chains: [sepolia, optimismSepolia, baseSepolia, arbitrumSepolia, unichainSepolia, soneiumMinato],
-  ssr: true,
+	appName: "My RainbowKit App",
+	projectId: "0e50ad124798913a4af212355f956d06",
+	chains: [
+		sepolia,
+		optimismSepolia,
+		baseSepolia,
+		arbitrumSepolia,
+		unichainSepolia,
+		soneiumMinato,
+	],
+	ssr: true,
 });
 
 function MyApp({ Component, pageProps }) {
-  const queryClient = new QueryClient();
-  return (
-    <Provider store={Store}>
-      <WagmiProvider config={config}>
-        <QueryClientProvider client={queryClient}>
-          <RainbowKitProvider theme={darkTheme()} coolMode={true}>
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          </RainbowKitProvider>
-        </QueryClientProvider>
-      </WagmiProvider>
-    </Provider>
-  );
+	const queryClient = new QueryClient();
+	return (
+		<Provider store={Store}>
+			<WagmiProvider config={config}>
+				<QueryClientProvider client={queryClient}>
+					<RainbowKitProvider
+						theme={lightTheme({
+							accentColor: "#00bcd4",
+							accentColorForeground: "white",
+							borderRadius: "large",
+						})}
+						coolMode={true}
+					>
+						<Layout>
+							<Component {...pageProps} />
+						</Layout>
+					</RainbowKitProvider>
+				</QueryClientProvider>
+			</WagmiProvider>
+		</Provider>
+	);
 }
 
 export default MyApp;
